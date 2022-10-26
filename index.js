ca9a7bf6e8090aafc72e86903ec5c9d05e36d3d7
yasinarafat39 <yasinarafat2557@gmail.com>
2022-10-27 00:09:47 +0600

    Implement a Server and running this server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+const Port = process.env.Port || 5000;
+
+const courses = require("./Data/courses.json");
+
+
+app.get("/", (req, res) => {
+    res.send("Now Server is running")
+})
+
+
+
+app.listen(Port, () => {
+    console.log("Server is running:", Port);
+})
