9c22111cecce4e9adc90afce614784a26d0fdcf9
yasinarafat39 <yasinarafat2557@gmail.com>
2022-10-27 00:19:03 +0600

    Create an API to get Details of our course with course id.

--- index.js
+++ index.js
@@ -10,14 +10,22 @@ const courses = require("./Data/courses.json");
 
 
 app.get("/", (req, res) => {
-    res.send("Now Server is running")
-})
+    res.send("Now Server is running");
+});
 
 app.get("/courses", (req, res) => {
     res.send(courses);
-})
+});
 
+app.get("course/:id", (req, res) => {
+    const id = req.params.id;
+    const getCourse = courses.find(c => c.id == id);
+    if (!getCourse) {
+        res.send("No Data Found");
+    }
+    res.send(getCourse);
+});
 
 app.listen(Port, () => {
     console.log("Server is running:", Port);
-})
+});
